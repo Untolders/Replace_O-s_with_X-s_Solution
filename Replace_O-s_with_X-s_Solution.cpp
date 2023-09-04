300e3bc4374d16a900a9f0e2af2785879db2e0b6
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-09-04 20:58:40 +0530

    Create Replace_O-s_with_X-s_Solution.cpp

---
+++ Replace_O-s_with_X-s_Solution.cpp
@@ -0,0 +1,136 @@
+**********************************************************************************  Question  ************************************************************************************************
+
+Given a matrix mat of size N x M where every element is either 'O' or 'X'. Replace all 'O' with 'X' that are surrounded by 'X'.
+
+A 'O' (or a set of 'O') is considered to be surrounded by 'X' if there are 'X' at locations just below, just above, just left and just right of it.
+
+Example 1:
+
+Input: 
+n = 5, m = 4
+mat = {{'X', 'X', 'X', 'X'}, 
+       {'X', 'O', 'X', 'X'}, 
+       {'X', 'O', 'O', 'X'}, 
+       {'X', 'O', 'X', 'X'}, 
+       {'X', 'X', 'O', 'O'}}
+Output: 
+ans = {{'X', 'X', 'X', 'X'}, 
+       {'X', 'X', 'X', 'X'}, 
+       {'X', 'X', 'X', 'X'}, 
+       {'X', 'X', 'X', 'X'}, 
+       {'X', 'X', 'O', 'O'}}
+Explanation: 
+Following the rule the above matrix is the resultant matrix. 
+Example 2:
+
+ 
+
+Input: 
+n = 5, m = 4
+mat = {{'X', 'O', 'X', 'X'}, 
+       {'X', 'O', 'X', 'X'}, 
+       {'X', 'O', 'O', 'X'}, 
+       {'X', 'O', 'X', 'X'}, 
+       {'X', 'X', 'O', 'O'}}
+Output: 
+ans = {{'X', 'O', 'X', 'X'}, 
+       {'X', 'O', 'X', 'X'}, 
+       {'X', 'O', 'O', 'X'}, 
+       {'X', 'O', 'X', 'X'}, 
+       {'X', 'X', 'O', 'O'}}
+Explanation: 
+Following the rule the above matrix is the resultant matrix.
+Your Task:
+You do not need to read input or print anything. Your task is to complete the function fill() which takes N, M and mat as input parameters ad returns a 2D array representing the resultant matrix.
+
+Expected Time Complexity: O(n*m)
+Expected Auxiliary Space: O(n*m)
+
+Constraints:
+1 ≤ n, m ≤ 500
+
+
+**********************************************************************************  Solution  ************************************************************************************************
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+    private:
+    void dfs(int i,int j,vector<vector<int>> &visit,vector<vector<char>> &mat){
+        visit[i][j] = 1;
+        
+        int row[] = {-1,0,1,0};
+        int col[] = {0,1,0,-1};
+        
+        for(int k=0;k<4;k++){
+            int nr = i+row[k];
+            int nc = j+col[k];
+            
+            if(nr < 0 || nr >= mat.size() || nc < 0 || nc >= mat[0].size())
+                continue;
+                
+            if(!visit[nr][nc] && mat[nr][nc]=='O'){
+                dfs(nr,nc,visit,mat);
+                visit[nr][nc] = 1;
+            }
+        }
+    }
+public:
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+        // code here
+           vector<vector<char>> res=mat;
+        vector<vector<int>> visit(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 || i==n-1 || j==0 || j==m-1){
+                    if(mat[i][j]=='O' && !visit[i][j])
+                        dfs(i,j,visit,mat);
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(visit[i][j]==0){
+                    res[i][j] = 'X';
+                }
+                else res[i][j] = 'O';
+            }
+        }   
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n, vector<char>(m, '.'));
+        for(int i = 0;i < n;i++)
+            for(int j=0; j<m; j++)
+                cin>>mat[i][j];
+        
+        Solution ob;
+        vector<vector<char>> ans = ob.fill(n, m, mat);
+        for(int i = 0;i < n;i++) {
+            for(int j = 0;j < m;j++) {
+                cout<<ans[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
